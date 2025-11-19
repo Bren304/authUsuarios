39020f2bdd202a2165052dd962fb64f4cc5a8d7f
Bren <jhaderjha26@gmail.com>
2025-11-19 14:42:40 -0500

    Implementacion de vistas, cierre de sesiones db

--- config/connection.py
+++ config/connection.py
@@ -9,7 +9,19 @@ db = SQLAlchemy()
 
 DATABASE_URL = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
+# En tu connection.py, asegúrate de cerrar conexiones correctamente
 def init_app(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+        'pool_size': 2,
+        'max_overflow': 1,
+        'pool_recycle': 180,  # 3 minutos
+        'pool_pre_ping': True,
+        'pool_timeout': 15
+    }
     db.init_app(app)
+    
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        db.session.remove()
