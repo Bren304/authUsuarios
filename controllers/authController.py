4035e52e0c7937b12ca890b61ad3f004e855e950
Bren <jhaderjha26@gmail.com>
2025-11-19 15:02:19 -0500

    cambio en controlador

--- controllers/authController.py
+++ controllers/authController.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from config.connection import db
+from middleware.authMiddleware import token_required
 from models.users import User
 import jwt
 import datetime
